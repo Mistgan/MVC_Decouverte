9a92d79f9397fc06770826576df91e9c443eada6
Rafiki <kevin.fontaine@efrei.net>
2022-03-15 19:39:20 +0100

    HololiveController
    transfering data management in the controller

--- MvcTest/Controllers/HololiveController.cs
+++ MvcTest/Controllers/HololiveController.cs
@@ -1,17 +1,18 @@
 using System.Net;
 using System.Text;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace MvcTest.Controllers;
 
 public class HololiveController : Controller
 {
+    string[] ignore = new[] {"free", "chat", "schedule", "weekly", "pretend"};
     /* TODO
-            Retirer les free chat 
-            Mettre en noir le fond
             Compresser la liste des live => dropdown
-            Faire la meme pour twitch et vshoja
+            Faire la meme pour twitch et vshojo
      */
     
     public IActionResult HololiveIndex()
@@ -37,27 +38,62 @@ public class HololiveController : Controller
     }
     
     // GET: hololive/live
-    public async Task<IEnumerable<JToken>> GetLive()
+    public async Task<JToken> GetLive()
     {
         Console.OutputEncoding = Encoding.UTF8;
         var data = await GetAll();
-        var live = data["live"];
-        return live.AsEnumerable();
+        JContainer live = (JContainer) data["live"];
+        foreach (var token in live)
+        {
+            token["title"] = (JToken) Translate(token["title"].ToString());
+            
+            Console.WriteLine("TYPE : " + token.GetType());
+            if (ignore.Any(token["title"].ToString().ToLower().Contains))
+            {
+                //Console.WriteLine("TO BE REMOVE : " + token);
+                
+            }
+            
+        }
+        return live;
     }
     
     // GET: hololive/upcoming
     public async Task<JToken?> GetUpcoming()
     {
         var data = await GetAll();
-        var live = data["upcoming"];
-        return live;
+        var upcoming = data["upcoming"];
+
+        return upcoming;
     }
     
     // GET: hololive/ended
     public async Task<JToken?> GetEnded()
     {
         var data = await GetAll();
-        var live = data["ended"];
-        return live;
+        var ended = data["ended"];
+
+        return ended;
+    }
+    
+    
+    // Translate to 
+    public string Translate(string input)
+    {
+        HttpClient client = new HttpClient();
+        var from = "auto";
+        var to = "en";
+        var text = HttpUtility.UrlEncode(input);
+        var url = String.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}", from, to, text);
+        var request = client.GetStringAsync(url).Result;
+        var jsonData = JsonConvert.DeserializeObject(request);
+        var lang = ((JArray) jsonData).Last.Last.Last;
+        if (lang.ToString() == "en")
+        {
+            return input;
+        }
+        var cr = ((JArray) jsonData).First.First.First.ToString().Replace("]", "】");
+        var cl = cr.Replace("[", "【");
+        return cl;
     }
 }
--- MvcTest/wwwroot/js/site.js
+++ MvcTest/wwwroot/js/site.js
@@ -1,10 +1,12 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-setInterval(refreshLiveList, 10000);
 
 function getId(id){
-    $("#video").attr("src", "https://www.youtube.com/embed/"+id+"?autoplay=1&rel=0");
+    $("#video").attr("src", "https://www.youtube.com/embed/"+id+"?autoplay=0&rel=0");
     $("#chat").attr("src", "https://www.youtube.com/live_chat?v="+id+"&embed_domain=localhost");
+    $( "#liveDiv" ).load(window.location.href + " #liveDiv" );
+    $( "#upcomingDiv" ).load(window.location.href + " #upcomingDiv" );
+    
 }
 
 
